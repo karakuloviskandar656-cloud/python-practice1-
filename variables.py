b7029c681c6f4299663765f518c38dcecd91bd4d
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-12 23:20:19 +0500

    Add variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+# variables.py
+
+# Assigning values to variables
+name = "Iskandar"
+age = 20
+height = 1.75
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# Changing a variable value
+age = age + 1
+print("Next year, age will be:", age)
