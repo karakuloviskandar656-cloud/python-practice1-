1fbe3d287f7e6e9ee367150eb98a0905cc762091
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 11:36:55 +0500

    Add Number.py with examples of data types

---
+++ Practice1/python-basics/Number.py
@@ -0,0 +1,40 @@
+x = 1 #int 
+y = 2.8 #float 
+z = 1j #complex
+
+#also if  we want to know which type is that number we can use func type()
+print(type(x))
+print(type(y))
+print(type(z))
+
+#example for integers 
+a = 1 
+b = 123421874917485
+c = -23418
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Example for float
+q = 1.10
+w = 1.0
+e = -35.59
+
+print(type(q))
+print(type(w))
+print(type(e))
+
+#Example for complex
+t= 3+5j
+u = 5j
+i = -5j
+
+print(type(t))
+print(type(u))
+print(type(i))
+
+# We can take random in some interval using func random by implying import
+import random
+print(random.randrange(1, 10))
+
