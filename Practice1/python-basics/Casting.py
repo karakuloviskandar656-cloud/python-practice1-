846e2ff04cabf09efb7ac6005c73bd41f34e479a
karakuloviskandar656-cloud <karakuloviskandar656@gmail.com>
2026-03-17 11:44:31 +0500

    Add Casting.py for type conversion examples

---
+++ Practice1/python-basics/Casting.py
@@ -0,0 +1,12 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+a = float(1)     # a will be 1.0
+b = float(2.8)   # b will be 2.8
+c = float("3")   # c will be 3.0
+d = float("4.2") # d will be 4.2
+
+q = str("s1") # q will be 's1'
+w = str(2)    # w will be '2'
+e = str(3.0)  # e will be '3.0'
